ce8add4ce49875a8f959f4a21367cfe87c62d13e
kamil <ghost150503@gmail.com>
2023-10-26 01:08:56 +0200

    Texture object

--- shaders/texture.hpp
+++ shaders/texture.hpp
@@ -16,13 +16,9 @@ public:
 
     Texture(const char* image, const char* texType, GLuint slot, GLenum format, GLenum pixelType);
 
-    // Assigns a texture unit to a texture
     void texUnit(Shader& shader, const char* uniform, GLuint unit);
-    // Binds a texture
     void Bind();
-    // Unbinds a texture
     void Unbind();
-    // Deletes a texture
     void Delete();
 };
 #endif
